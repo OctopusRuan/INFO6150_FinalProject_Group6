2cc1ce51faa2dabe30ec4c20d031f7af48970ba5
Liviaiazhang <zhang.di2@northeastern.edu>
2025-04-14 18:58:16 -0400

    initial commmit for zhangdi branch

---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    match: /^[a-zA-Z0-9._%+-]+@northeastern\.edu$/ 
+  },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
